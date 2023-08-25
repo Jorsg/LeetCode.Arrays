8ce9cae6a7bcd9792253a18b593d534ecc5c7191
Jorman Garcia <jagarcia@lfs-inc.com>
2023-08-25 11:37:48 -0400

    Exercise of Arrays

--- LeetCode.Arrays/Program.cs
+++ LeetCode.Arrays/Program.cs
@@ -1,7 +1,17 @@
-﻿public class Program
+﻿using LeetCode.Arrays;
+
+public class Program
 {
 	private static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		DVD[] dvdCollection = new DVD[3];
+
+		dvdCollection[0] = new DVD("Pelicula 1", 2000, "Director 1");
+		dvdCollection[1] = new DVD("Pelicula 2", 2010, "Director 2");
+		dvdCollection[2] = new DVD("Pelicula 3", 2020, "Director 3");
+
+
+		foreach (var dvd in dvdCollection) Console.WriteLine(dvd.toStrinPrint());
+			
 	}
 }
