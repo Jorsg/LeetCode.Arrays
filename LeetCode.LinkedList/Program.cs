3b2323b060ac9ef1c489ffa00e73c1b9c422a875
Jorman Garcia <jagarcia@lfs-inc.com>
2023-09-05 21:48:57 -0400

    Add new ArrayExcersice and add new project Linked list

---
+++ LeetCode.LinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.LinkedList;
+
+public class Program
+{
+	private static void Main(string[] args)
+	{
+		MyLinkedList myLinkedList = new MyLinkedList();
+		//myLinkedList.AddAtHead(1);
+		//myLinkedList.AddAtTail(3);
+		//myLinkedList.AddAtIndex(1, 2);
+		Console.WriteLine(myLinkedList.Get(1));
+		//myLinkedList.DeleteAtIndex(1);
+		myLinkedList.Get(1);
+	}
+}
