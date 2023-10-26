c42c9a79695234bde099c5fd10419f4c8e40dd82
Jorman Garcia <jagarcia@lfs-inc.com>
2023-10-26 18:02:38 -0400

    add new exercise 557. Reverse Words in a String III

--- LeetCode.ArrayString/ReserveString.cs
+++ LeetCode.ArrayString/ReserveString.cs
@@ -29,7 +29,11 @@ namespace LeetCode.ArrayString
 			}
 		}
 
-
+		//151. Reverse Words in a String
+		//Given an input string s, reverse the order of the words.
+		//A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+		//Return a string of the words in reverse order concatenated by a single space.
+		//Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 		public string ReverseWords(string s)
 		{
 			//Runtime: 80 ms
@@ -37,6 +41,27 @@ namespace LeetCode.ArrayString
 			string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 			Array.Reverse(words);
 			return string.Join(" ", words);
+		}
+
+		//557. Reverse Words in a String III
+		//Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+		//Example 1:
+		//Input: s = "Let's take LeetCode contest"
+		//Output: "s'teL ekat edoCteeL tsetnoc"
+		public string ReverseWordsIII(string s)
+		{
+			//Runtime: 81 ms
+			//Memory Usage: 47.2 MB
+			string[] words = s.Split();
+			string[] reversedWords = new string[words.Length];
+			for (int i = 0; i < words.Length; i++)
+			{
+				char[] wordChar = words[i].ToCharArray();
+				Array.Reverse(wordChar);
+				reversedWords[i] = new string(wordChar);
+			}
+			return string.Join("", reversedWords);
 		}
 	}
 }
