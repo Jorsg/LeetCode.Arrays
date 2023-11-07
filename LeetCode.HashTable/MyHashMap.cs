8cd2b7b6962ecdc9f6b4c9240e29bd30d49964af
Jorman Garcia <jagarcia@lfs-inc.com>
2023-11-06 22:25:57 -0500

    Add excersice 705 Design HashSet and HashMap, alse add new project LeetCode Microsoft by do excersice

---
+++ LeetCode.HashTable/MyHashMap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{	
+	public class MyHashMap
+	{
+		int[] a;
+		MyHashMap()
+		{
+			a = new int[1000001];
+			Array.Fill(a,-1);
+		}
+
+		void put(int key, int value)
+		{
+			a[key] = value;
+		}
+
+		int get(int key)
+		{
+			return a[key];
+		}
+
+		void remove(int key)
+		{
+			a[key]= -1;
+		}
+
+	}
+}
