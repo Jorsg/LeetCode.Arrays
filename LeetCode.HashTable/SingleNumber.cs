bfd984a52992ced2e03a1286b7818f4dcb8d992d
Jorman Garcia <jagarcia@lfs-inc.com>
2023-11-08 14:38:47 -0500

    Add new excersice 217. Contains Duplicate, 136. single number and 349. Intersection of Two Arrays

---
+++ LeetCode.HashTable/SingleNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+	public static class SingleNumber
+	{
+		//136. Single Number
+		//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+		//You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+		//Example 1:
+		//Input: nums = [2,2,1]
+		//Output: 1
+		public static int SingleNumberExcer(int[] nums)
+		{
+			//Runtime: 107 ms
+			//Memory Usage: 43.9 MB
+			int single = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+
+				single ^= nums[i]; // XOR
+
+			}
+
+			return single;
+		}
+	}
+}
