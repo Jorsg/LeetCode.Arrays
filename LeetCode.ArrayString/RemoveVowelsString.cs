cd7fc039fe3b8b8487f2bf20f1c0f5faa1ee93cc
Jorman Garcia <jagarcia@lfs-inc.com>
2023-11-20 16:49:23 -0500

    Add new exercise 599. Minumun Index Sum of Two List and Remove vowels

---
+++ LeetCode.ArrayString/RemoveVowelsString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ArrayString
+{
+	public static class RemoveVowelsString
+	{
+		//1119. Remove Vowels from a String
+		//Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+
+		//Example 1:
+		//Input: s = "leetcodeisacommunityforcoders"
+		//Output: "ltcdscmmntyfrcdrs"
+		//Example 2:
+	
+		//Input: s = "aeiou"
+		//Output:
+
+		public static string RemoveVowels(string s)
+		{
+			string vowels = "aeiou";
+			return s = new string(s.Where(c => !vowels.Contains(c)).ToArray());
+		}
+	}
+}
---
+++ LeetCode.HashTable/MiniIndexSumTwoList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.HashTable
+{
+	internal class MiniIndexSumTwoList
+	{
+
+		//599. Minimum Index Sum of Two Lists
+		//Given two arrays of strings list1 and list2, find the common strings with the least index sum.
+		//A common string is a string that appeared in both list1 and list2.
+		//A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.
+		//Return all the common strings with the least index sum. Return the answer in any order.
+
+		//Example 1:
+
+		//Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+		//Output: ["Shogun"]
+		//Explanation: The only common string is "Shogun".
+
+		//Example 2:
+		//Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+		//Output: ["Shogun"]
+		//Explanation: The common string with the least index sum is "Shogun" with index sum = (0 + 1) = 1.
+
+		public string[] FindRestaurant(string[] list1, string[] list2)
+		{
+			//Runtime: 203 ms
+			//Memory Usage: 63.4 MB
+
+			Dictionary<string, int> dict = new Dictionary<string, int>();
+			for (int i = 0; i < list1.Length; i++)
+			{
+				for (int j = 0; j < list2.Length; j++)
+				{
+					if (list1[i] == list2[j])
+					{
+						dict.Add(list1[i], i + j);
+					}
+				}
+			}
+			int min = dict.Min(x => x.Value);
+			return dict.Where(x => x.Value == min).Select(x => x.Key).ToArray();
+		}
+	}
+}
