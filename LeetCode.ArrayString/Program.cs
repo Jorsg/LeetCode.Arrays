513cfb13cb764f460fa4f717b4521b0368ce40ea
Jorman Garcia <jagarcia@lfs-inc.com>
2023-12-10 17:14:40 -0500

    Add new exercise about SQL

--- LeetCode.ArrayString/Program.cs
+++ LeetCode.ArrayString/Program.cs
@@ -8,7 +8,13 @@ public class Program
 
 		//int[] array = IntroductionArray.PlusOne(nums);
 		//foreach (int i in array) Console.WriteLine(i);
-		string a = "11", b = "1";
-		Console.WriteLine(AddBinary.AddBinaryExercesi(a,b));
+		//string a = "11", b = "1";
+		//Console.WriteLine(AddBinary.AddBinaryExercesi(a,b));
+		string[] words = { "abc", "bcd", "aaaa", "cbc" };
+		char x = 'a';
+		//Console.WriteLine(FindWordsContainingCharacter.FindWordsContaining(words, x));
+		int result = 3 + 1 * 5 / 2;
+		Console.WriteLine(result);
+		
 	}
 }
