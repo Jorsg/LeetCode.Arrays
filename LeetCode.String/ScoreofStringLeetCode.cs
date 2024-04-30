52737cfcaf6ad5aaf7c1d01d3672e550a4196751
Jorman Garcia <jagarcia@lfs-inc.com>
2024-04-29 21:10:31 -0400

    Add new project LeetCode.String, this project I'm going to posted all execersies that I made on LeetCode

---
+++ LeetCode.String/ScoreofStringLeetCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.String
+{
+	public static class ScoreofStringLeetCode
+	{
+		public static int ScoreOfString(string s)
+		{
+			var score = 0;
+
+			for (int i = 1; i < s.Length; i++)
+			{
+				score += Math.Abs(s[i] - s[i - 1]);
+			}
+			return score;
+		}
+	}
+}
