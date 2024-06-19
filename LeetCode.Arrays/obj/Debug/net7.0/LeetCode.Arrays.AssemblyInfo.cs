ccfff9084dca59bcd45a75578cbfb960cc898fba
Jorman Garcia <jagarcia@lfs-inc.com>
2024-06-18 22:58:14 -0400

    add new exercese from LeetCode 2037. Minimum Number of Moves to Seat Everyone

--- LeetCode.Arrays/obj/Debug/net7.0/LeetCode.Arrays.AssemblyInfo.cs
+++ LeetCode.Arrays/obj/Debug/net7.0/LeetCode.Arrays.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c609be63285b35ef7c13dc85b1dd47cdecf3f8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfdfa07990c5670542b004053d647adc867b0ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
