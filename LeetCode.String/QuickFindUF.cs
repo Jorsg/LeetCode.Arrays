db0c0e2498bda7626049bd9b175b8bb43a796ab4
Jorman Garcia <jagarcia@lfs-inc.com>
2024-07-18 21:10:32 -0400

    Add new algorithm Quick Find

---
+++ LeetCode.String/QuickFindUF.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.String
+{
+	public static class QuickFindUF
+	{
+		private static int[] id;
+
+		public static void Init(int n)
+		{
+			id = new int[n];
+			for (int i = 0; i < n; i++)
+			{
+				id[i] = i;
+			}
+		}
+
+		public static bool Connected(int p, int q)
+		{
+			return id[p] == id[q];
+		}
+
+		public static void Union(int p, int q)
+		{
+			int pid = id[p];
+			int qid = id[q];
+			for (int i = 0; i < id.Length; i++)
+			{
+				if (id[i] == pid)
+				{
+					id[i] = qid;
+				}
+			}
+		}
+	}
+}
--- LeetCode.String/obj/Debug/net8.0/LeetCode.String.AssemblyInfo.cs
+++ LeetCode.String/obj/Debug/net8.0/LeetCode.String.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.String")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181830aaa8c8dd7e6f7f34fcfc308eba0a7e27e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f042fefa2ad98b36026570e0203ea8d5e74cf73")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.String")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.String")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
