dce65805e20a83ea3a1c602afcad2b2ef92cf678
Jorman Garcia <jagarcia@lfs-inc.com>
2024-08-12 22:35:31 -0400

    Add new algorithm, exercise String 1.2 Check Permitation CRACKING CODIGN INTERVIEW

--- LeetCode.String/obj/Debug/net8.0/LeetCode.String.AssemblyInfo.cs
+++ LeetCode.String/obj/Debug/net8.0/LeetCode.String.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode.String")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db0c0e2498bda7626049bd9b175b8bb43a796ab4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d73a992959e3881547a5ffd53bcfda698029cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode.String")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode.String")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
